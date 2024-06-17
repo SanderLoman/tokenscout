2c075dad055d8e79859e3050340e776a828efe64
SanderLoman <sanderfeitsma13@gmail.com>
2024-06-17 14:58:00 +0200

    feat: updating crates

--- tokenscout/src/main.rs
+++ tokenscout/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use ethers::providers::{Provider, Ws};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let ws_url = env::var("WS_TOKENSCOUT_API_KEY").expect("WS_TOKENSCOUT_API_KEY not set");
+
+    let _provider = Provider::<Ws>::connect(ws_url.as_str())
+        .await
+        .expect("could not connect to the provider");
 }
