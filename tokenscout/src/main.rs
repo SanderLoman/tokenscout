ceffffe5a47b9007e61cf16870e07236e5ed2796
SanderLoman <sanderfeitsma13@gmail.com>
2024-06-17 23:01:48 +0200

    chore: Remove unused code and update dependencies

--- tokenscout/src/main.rs
+++ tokenscout/src/main.rs
@@ -1,14 +1,2 @@
-use dotenv::dotenv;
-use ethers::providers::{Provider, Ws};
-use std::env;
-
 #[tokio::main]
-async fn main() {
-    dotenv().ok();
-
-    let ws_url = env::var("WS_TOKENSCOUT_API_KEY").expect("WS_TOKENSCOUT_API_KEY not set");
-
-    let _provider = Provider::<Ws>::connect(ws_url.as_str())
-        .await
-        .expect("could not connect to the provider");
-}
+async fn main() {}
