8bc2b065e5ea2c9b278a6216e6871b9a15580da0
SanderLoman <sanderfeitsma13@gmail.com>
2024-06-19 21:10:53 +0200

    chore: Refactor logger module and update dependencies

--- common/logger/src/lib.rs
+++ common/logger/src/lib.rs
@@ -1,28 +1,4 @@
-use slog::{o, Drain, FnValue, Level, Logger, Record};
-use slog_async;
-use slog_term::{self, FullFormat, TermDecorator};
-
-struct RangeLevelFilter<D> {
-    levels: Vec<Level>,
-    drain: D,
-}
-
-impl<D> Drain for RangeLevelFilter<D>
-where
-    D: Drain<Ok = ()>,
-{
-    type Ok = ();
-    type Err = D::Err;
-
-    fn log(&self, record: &Record, values: &slog::OwnedKVList) -> Result<Self::Ok, Self::Err> {
-        if self.levels.contains(&record.level()) {
-            self.drain.log(record, values)?;
-            Ok(())
-        } else {
-            Ok(())
-        }
-    }
-}
+use slog::Level;
 
 pub fn parse_verbosity(verbosity: u64) -> Vec<Level> {
     match verbosity {
@@ -47,21 +23,4 @@ pub fn parse_verbosity(verbosity: u64) -> Vec<Level> {
         ],
         _ => vec![Level::Info, Level::Warning, Level::Error, Level::Critical],
     }
-}
-
-pub fn create_logger(levels: Vec<Level>) -> Logger {
-    let decorator = TermDecorator::new().build();
-    let drain = FullFormat::new(decorator)
-        .use_custom_timestamp(slog_term::timestamp_local)
-        .build()
-        .fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
-    let drain = RangeLevelFilter { levels, drain }.fuse();
-
-    Logger::root(
-        drain,
-        o!(
-            "location" => FnValue(move |info| format!("{}::{}::{}", info.file(), info.module(), info.line()))
-        ),
-    )
 }
--- tokenscout/src/main.rs
+++ tokenscout/src/main.rs
@@ -1,13 +1,8 @@
 pub mod importer;
 pub mod networking;
 
-use clap::ArgAction;
-use clap::{Arg, Command};
-use logger::{create_logger, parse_verbosity};
-use networking::initialize_network_manager;
-use networking::EthNode;
+use clap::{Arg, ArgAction, Command};
 use slog::{crit, debug, error, info, trace, warn};
-use std::sync::Arc;
 use tokenscout_version::VERSION;
 
 #[tokio::main]
@@ -32,22 +27,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .action(ArgAction::Count)
                 .global(true)
                 .display_order(0),
-        );
-
-    let matches = matches.get_matches();
+        )
+        .get_matches();
 
     let verbosity = matches.get_count("verbosity");
-    let levels = parse_verbosity(verbosity.into());
-
-    let logger = create_logger(levels);
-    let logger = Arc::new(logger);
-
-    info!(logger, "Starting tokenscout"; "version" => VERSION);
-    warn!(logger, "This is a warning message");
-    error!(logger, "This is an error message");
-    crit!(logger, "This is a critical message");
-    debug!(logger, "This is a debug message");
-    trace!(logger, "This is a trace message");
 
     Ok(())
 }
