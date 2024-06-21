c1bfa2495132e98c59ed8d289169e2bed7b1b183
SanderLoman <sanderfeitsma13@gmail.com>
2024-06-21 23:24:53 +0200

    refactor: Comment out unused code in config.rs

--- tokenscout/src/config.rs
+++ tokenscout/src/config.rs
@@ -1,10 +1,10 @@
 use slog::Logger;
 
 pub struct Environment {
-    pub analyzer: Analyzer,
-    pub database: Database,
-    pub ranker: Ranker,
-    pub telegram: Telegram,
-    pub config: TokenscoutConfig,
+    // pub analyzer: Analyzer,
+    // pub database: Database,
+    // pub ranker: Ranker,
+    // pub telegram: Telegram,
+    // pub config: TokenscoutConfig,
     pub logger: Logger,
 }
