2837d6cb11404d0242135ec365777b24c2284660
Sander Loman <sanderfeitsma13@gmail.com>
2024-06-28 00:20:35 +0200

    Dependency changes (#66)
    * chore: Update dependencies
    
    * chore: Update tracing and eyre dependencies

--- tokenscout/src/config.rs
+++ tokenscout/src/config.rs
@@ -1,10 +1,8 @@
-use slog::Logger;
-
 pub struct Environment {
     // pub analyzer: Analyzer,
     // pub database: Database,
     // pub ranker: Ranker,
     // pub telegram: Telegram,
     // pub config: TokenscoutConfig,
-    pub logger: Logger,
+    // pub logger: Logger,
 }
--- tokenscout/src/lib.rs
+++ tokenscout/src/lib.rs
@@ -4,7 +4,6 @@ pub mod importer;
 pub mod networking;
 
 use clap::{Arg, ArgAction, Command};
-use slog::{crit, debug, error, info, trace, warn};
 
 impl TokenscoutConfig {
     pub fn new() {}
