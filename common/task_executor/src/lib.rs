e64c32685b92b2406e0094e9f39f8a440947fad0
SanderLoman <sanderfeitsma13@gmail.com>
2024-06-30 22:29:41 +0200

    feat: Add metrics module and task_executor

--- common/task_executor/src/lib.rs
+++ common/task_executor/src/lib.rs
@@ -1,14 +1,167 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use futures::channel::mpsc::Sender;
+use futures::prelude::*;
+use std::sync::Weak;
+use tokio::runtime::{Handle, Runtime};
+use tracing::{debug, info, trace, warn};
+
+pub use tokio::task::JoinHandle;
+
+mod metrics;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum ShutdownReason {
+    Success(&'static str),
+    Failure(&'static str),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl ShutdownReason {
+    pub fn message(&self) -> &'static str {
+        match self {
+            ShutdownReason::Success(msg) | ShutdownReason::Failure(msg) => msg,
+        }
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Clone)]
+pub enum HandleProvider {
+    Runtime(Weak<Runtime>),
+    Handle(Handle),
+}
+
+impl From<Handle> for HandleProvider {
+    fn from(handle: Handle) -> Self {
+        HandleProvider::Handle(handle)
+    }
+}
+
+impl From<Weak<Runtime>> for HandleProvider {
+    fn from(weak_runtime: Weak<Runtime>) -> Self {
+        HandleProvider::Runtime(weak_runtime)
+    }
+}
+
+impl HandleProvider {
+    pub fn handle(&self) -> Option<Handle> {
+        match self {
+            HandleProvider::Runtime(weak_runtime) => weak_runtime
+                .upgrade()
+                .map(|runtime| runtime.handle().clone()),
+            HandleProvider::Handle(handle) => Some(handle.clone()),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct TaskExecutor {
+    handle_provider: HandleProvider,
+    exit: async_channel::Receiver<()>,
+    signal_tx: Sender<ShutdownReason>,
+}
+
+impl TaskExecutor {
+    pub fn new<T: Into<HandleProvider>>(
+        handle: T,
+        exit: async_channel::Receiver<()>,
+        signal_tx: Sender<ShutdownReason>,
+    ) -> Self {
+        Self {
+            handle_provider: handle.into(),
+            exit,
+            signal_tx,
+        }
+    }
+
+    pub fn clone_with_name(&self, service_name: String) -> Self {
+        TaskExecutor {
+            handle_provider: self.handle_provider.clone(),
+            exit: self.exit.clone(),
+            signal_tx: self.signal_tx.clone(),
+        }
+    }
+
+    pub fn spawn_ignoring_error(
+        &self,
+        task: impl Future<Output = Result<(), ()>> + Send + 'static,
+        name: &'static str,
+    ) {
+        self.spawn(task.map(|_| ()), name)
+    }
+
+    pub fn spawn(&self, task: impl Future<Output = ()> + Send + 'static, name: &'static str) {
+        if let Some(task_handle) = self.spawn_handle(task, name) {
+            self.spawn_monitor(task_handle, name)
+        }
+    }
+
+    fn spawn_monitor<R: Send>(
+        &self,
+        task_handle: impl Future<Output = Result<R, tokio::task::JoinError>> + Send + 'static,
+        name: &'static str,
+    ) {
+        let mut shutdown_sender = self.shutdown_sender();
+        if let Some(handle) = self.handle() {
+            handle.spawn(async move {
+                let timer = metrics::start_timer_vec(&metrics::TASKS_HISTOGRAM, &[name]);
+                if let Err(join_error) = task_handle.await {
+                    if let Ok(_panic) = join_error.try_into_panic() {
+                        warn!("Task panicked: {}", name);
+                        let _ = shutdown_sender
+                            .try_send(ShutdownReason::Failure("Panic (fatal error)"));
+                    }
+                }
+                drop(timer);
+            });
+        } else {
+            debug!("Couldn't spawn monitor task. Runtime shutting down");
+        }
+    }
+
+    pub fn spawn_handle<R: Send + 'static>(
+        &self,
+        task: impl Future<Output = R> + Send + 'static,
+        name: &'static str,
+    ) -> Option<tokio::task::JoinHandle<Option<R>>> {
+        let exit = self.exit();
+
+        if let Some(int_gauge) = metrics::get_int_gauge(&metrics::ASYNC_TASKS_COUNT, &[name]) {
+            int_gauge.inc();
+            if let Some(handle) = self.handle() {
+                Some(handle.spawn(async move {
+                    futures::pin_mut!(exit);
+                    let result = match future::select(Box::pin(task), exit).await {
+                        future::Either::Left((value, _)) => {
+                            trace!("Async task completed: {}", name);
+                            Some(value)
+                        }
+                        future::Either::Right(_) => {
+                            debug!("Async task shutdown, exit received: {}", name);
+                            None
+                        }
+                    };
+                    int_gauge.dec();
+                    result
+                }))
+            } else {
+                debug!("Couldn't spawn task. Runtime shutting down");
+                None
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn handle(&self) -> Option<Handle> {
+        self.handle_provider.handle()
+    }
+
+    pub fn exit(&self) -> impl Future<Output = ()> {
+        let exit = self.exit.clone();
+        async move {
+            let _ = exit.recv().await;
+        }
+    }
+
+    pub fn shutdown_sender(&self) -> Sender<ShutdownReason> {
+        self.signal_tx.clone()
     }
 }
--- common/tokenscout_metrics/src/lib.rs
+++ common/tokenscout_metrics/src/lib.rs
@@ -1,1 +1,85 @@
+use prometheus::HistogramOpts;
+use prometheus::Opts;
+use std::time::Duration;
 
+pub use prometheus::{
+    exponential_buckets, linear_buckets,
+    proto::{Metric, MetricFamily, MetricType},
+    Encoder, Gauge, GaugeVec, Histogram, HistogramTimer, HistogramVec, IntCounter, IntCounterVec,
+    IntGauge, IntGaugeVec, Result, TextEncoder, DEFAULT_BUCKETS,
+};
+
+pub fn gather() -> Vec<prometheus::proto::MetricFamily> {
+    prometheus::gather()
+}
+
+pub fn try_create_int_counter(name: &str, help: &str) -> Result<IntCounter> {
+    let opts = Opts::new(name, help);
+    let counter = IntCounter::with_opts(opts)?;
+    prometheus::register(Box::new(counter.clone()))?;
+    Ok(counter)
+}
+
+pub fn try_create_int_gauge(name: &str, help: &str) -> Result<IntGauge> {
+    let opts = Opts::new(name, help);
+    let gauge = IntGauge::with_opts(opts)?;
+    prometheus::register(Box::new(gauge.clone()))?;
+    Ok(gauge)
+}
+
+pub fn try_create_histogram(name: &str, help: &str) -> Result<Histogram> {
+    let opts = HistogramOpts::new(name, help);
+    let histogram = Histogram::with_opts(opts)?;
+    prometheus::register(Box::new(histogram.clone()))?;
+    Ok(histogram)
+}
+
+pub fn try_create_int_gauge_vec(
+    name: &str,
+    help: &str,
+    label_names: &[&str],
+) -> Result<IntGaugeVec> {
+    let opts = Opts::new(name, help);
+    let gauge_vec = IntGaugeVec::new(opts, label_names)?;
+    prometheus::register(Box::new(gauge_vec.clone()))?;
+    Ok(gauge_vec)
+}
+
+pub fn try_create_histogram_vec(
+    name: &str,
+    help: &str,
+    label_names: &[&str],
+) -> Result<HistogramVec> {
+    let opts = HistogramOpts::new(name, help);
+    let histogram_vec = HistogramVec::new(opts, label_names)?;
+    prometheus::register(Box::new(histogram_vec.clone()))?;
+    Ok(histogram_vec)
+}
+
+pub fn inc_counter(counter: &Result<IntCounter>) {
+    if let Ok(counter) = counter {
+        counter.inc();
+    }
+}
+
+pub fn set_gauge(gauge: &Result<IntGauge>, value: i64) {
+    if let Ok(gauge) = gauge {
+        gauge.set(value);
+    }
+}
+
+pub fn observe(histogram: &Result<Histogram>, value: f64) {
+    if let Ok(histogram) = histogram {
+        histogram.observe(value);
+    }
+}
+
+pub fn observe_duration(histogram: &Result<Histogram>, duration: Duration) {
+    if let Ok(histogram) = histogram {
+        histogram.observe(duration_to_f64(duration))
+    }
+}
+
+fn duration_to_f64(duration: Duration) -> f64 {
+    duration.as_secs() as f64 + f64::from(duration.subsec_nanos()) / 1e9
+}
